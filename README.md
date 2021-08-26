360cbc4262e085a1a697a02a3bc5fca1ac181780
unknown <quotary@hotmail.com>
2021-08-26 22:54:47 +0200

    feat: prepare code for leaderboard lists

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # partytop
+
+There is no command to get a list of all parties and their ranking. This plugin fixes that in a basic way.
--- src/main/java/pro/falco/partytop/PartyTopCommand.java
+++ src/main/java/pro/falco/partytop/PartyTopCommand.java
@@ -2,6 +2,7 @@ package pro.falco.partytop;
 
 import com.gmail.nossr50.datatypes.party.Party;
 import com.gmail.nossr50.party.PartyManager;
+import com.gmail.nossr50.util.text.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 import org.bukkit.command.Command;
@@ -10,23 +11,43 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class PartyTopCommand implements Listener, CommandExecutor {
     public boolean onCommand (@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
         if (sender instanceof Player) {
             final List<Party> parties = PartyManager.getParties();
 
-            parties.sort(Comparator.comparingInt(Party::getLevel).reversed());
-
-            parties.forEach((party) -> {
-                sender.sendMessage("Sorted: " + party.getName() + ", level: " + party.getLevel());
-            });
-
-            return true;
+            switch (args.length) {
+                case 0:
+                    print(1, sender);
+                    return true;
+                case 1:
+                    if (StringUtils.isInt(args[0])) {
+                        print(Math.abs(Integer.parseInt(args[0])), sender);
+                        return true;
+                    }
+            }
         }
-
         return false;
+    }
+
+    private void print(int page, @NotNull CommandSender sender) {
+        final List<Party> parties = PartyManager.getParties();
+        parties.sort(Comparator.comparingInt(Party::getLevel).reversed());
+
+        List<Party> partyList = new ArrayList<Party>();
+        IntStream.range(0, parties.size()).forEach(index -> {
+            partyList.add(parties.get(index));
+        });
+
+        sender.sendMessage("§e--mcMMO §9Party §eLeaderboard--");
+        partyList.forEach((party) -> {
+            sender.sendMessage("#. §a" + party.getName() + " - §f" + party.getLevel());
+        });
+        sender.sendMessage("§6Tip: Use §c/party info §6to see all of your party stats!");
     }
 }
